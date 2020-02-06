566ff0ffe0af97db8a8fd1ecb3f9ab547ef7030d
Satinders <satinder.singh@essential.net.au>
2020-02-06 16:19:14 +1100

    continued

---
+++ Application.UnitTests/Common/QueryTestBase.cs
@@ -0,0 +1,30 @@
+﻿using Persistence;
+using System;
+using AutoMapper;
+using Application.Common.Mappings;
+
+namespace Application.UnitTests.Common
+{
+    public class QueryTestBase : IDisposable
+    {
+        protected readonly ProductDbContext _context;
+        protected readonly IMapper _mapper;
+
+        public QueryTestBase()
+        {
+            _context = ProductDbContextFactory.Create();
+
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+        public void Dispose()
+        {
+            ProductDbContextFactory.Destroy(_context);
+        }
+    }
+}
--- Application.UnitTests/Products/Commands/UpdateProductCommandTests.cs
+++ Application.UnitTests/Products/Commands/UpdateProductCommandTests.cs
@@ -11,5 +11,18 @@ namespace Application.UnitTests.Products.Commands
 {
     public class UpdateProductCommandTests : CommandTestBase
     {
+        [Fact]
+        public async Task Handle_GivenExistingProduct_WhenUpdatingWithAValidName_ShouldUpdateProductName()
+        {
+            // Arrange
+            UpdateProductCommand request = new UpdateProductCommand {Id = new Guid("8BDEAB77-6BBC-43AE-9E07-2561660F4499"), Name = "Test Product", Description = "First Test Product", Price = 200.55m, DeliveryPrice = 20 };
+            var handler = new UpdateProductCommandHandler(_context);
+
+            //Act
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            //Assert
+            result.Name.ShouldBe("Test Product");
+        }
     }
 }
