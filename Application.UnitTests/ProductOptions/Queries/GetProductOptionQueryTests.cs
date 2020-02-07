0c9431f875e6b1d05b9808b65e6a17d63e257bb5
Satinders <satinder.singh@essential.net.au>
2020-02-07 15:36:18 +1100

    continued

---
+++ Application.UnitTests/ProductOptions/Queries/GetProductOptionQueryTests.cs
@@ -0,0 +1,51 @@
+﻿using Application.UnitTests.Common;
+using System;
+using Application.ProductOptions.Queries.GetProductOptions;
+using Shouldly;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Application.Common.Exceptions;
+
+namespace Application.UnitTests.ProductOptions.Queries
+{
+    public class GetProductOptionQueryTests : QueryTestBase
+    {
+        private readonly GetProductOptionQuery.GetProductOptionQueryHandler _queryHandler;
+
+        public GetProductOptionQueryTests()
+            : base()
+        {
+            _queryHandler = new GetProductOptionQuery.GetProductOptionQueryHandler(_context, _mapper);
+        }
+
+        [Fact]
+        public async Task Handle_GivenValidProductOptionId_GetsProductOption()
+        {
+            //Arrange
+            Guid productOptionId = new Guid("5BDEAB22-6BBC-43AE-9E07-2561660F4904");
+            var request = new GetProductOptionQuery
+            {
+                ProductOptionId = productOptionId
+            };
+
+            //Act
+            var result = await _queryHandler.Handle(request, CancellationToken.None);
+
+            //Assert
+            result.ShouldBeOfType<ProductOptionVm>();
+            result.Id.ShouldBe(productOptionId);
+        }
+
+        [Fact]
+        public async Task Handle_WhenTryingToGetDetailsOfNonExistingProductOptionId_ThrowsNotFoundException()
+        {
+            var request = new GetProductOptionQuery
+            {
+                ProductOptionId = new Guid()
+            };
+
+            await Assert.ThrowsAsync<NotFoundException>(() => _queryHandler.Handle(request, CancellationToken.None));
+        }
+    }
+}
---
+++ Application.UnitTests/ProductOptions/Queries/GetProductOptionsListQueryTests.cs
@@ -0,0 +1,28 @@
+﻿using Application.ProductOptions.Queries.GetProductOptionsList;
+using Application.UnitTests.Common;
+using Shouldly;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.UnitTests.ProductOptions.Queries
+{
+    public class GetProductOptionsListQueryTests : QueryTestBase
+    {
+        [Fact]
+        public async Task GetProductOptionsList()
+        {
+            //Arrange
+            var request = new GetProductOptionsListQuery();
+            var handler = new GetProductOptionsListQuery.GetProductOptionsListQueryHandler(_context, _mapper);
+            int minimumTestProductOptionsCount = 3;
+
+            //Act
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            //Assert
+            result.ShouldBeOfType<ProductOptionsListVm>();
+            result.ProductOptions.Count.ShouldBeGreaterThan(minimumTestProductOptionsCount);
+        }
+    }
+}
