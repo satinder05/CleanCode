ac53d08d7892f57606deb80aae37541d50ba6a68
Satinders <satinder.singh@essential.net.au>
2020-02-09 14:41:22 +1100

    struggle

---
+++ API.IntegrationTests/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace API.IntegrationTests.Controllers
+{
+    public class ProductsControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public ProductsControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/api/products")]
+        //[InlineData("/api/products/8F2E9176-35EE-4F0A-AE55-83023D2DB1A3")]
+        public async Task ApiRouteTest(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
